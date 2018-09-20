6263dbb69159c953a6241bed5f07d2e54ad2dafb
babonaresh <nbashaboena@gmail.com>
2018-09-19 19:43:00 -0500

    additional files and changes for Heroku

--- crm/models.py
+++ crm/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 
 # Create your models here.
 class Customer(models.Model):
