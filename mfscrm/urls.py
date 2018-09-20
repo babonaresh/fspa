9aba0a181315163c92a99242c8d22e0e38fa5cf3
babonaresh <nbashaboena@gmail.com>
2018-09-20 18:13:40 -0500

    updated

--- mfscrm/urls.py
+++ mfscrm/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.urls import path
 from django.contrib.auth import views
 from django.contrib.auth import views as auth_views
-from django.urls import path
+
 
 
 urlpatterns = [
